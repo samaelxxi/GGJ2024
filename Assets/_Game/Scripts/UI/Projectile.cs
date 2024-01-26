8516d7e9fa629dec0cd266c28cf57719f8891ffe
d.dankovskyi <d.dankovskyi@gmail.com>
2024-01-26 22:08:14 +0200

    mirror match + projectile start

---
+++ Assets/_Game/Scripts/UI/Projectile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Projectile : MonoBehaviour
+{
+    public float Speed = 1f;
+    Vector3 _target;
+
+    public void Init(Vector3 target)
+    {
+        _target = target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = _target - transform.position;
+        direction.Normalize();
+        transform.Translate(direction * Speed * Time.deltaTime);
+        
+    }
+}
--- Assets/_Game/Scripts/UI/UICharactersCardsManager.cs
+++ Assets/_Game/Scripts/UI/UICharactersCardsManager.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UICharactersCardsManager : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     [SerializeField] CharactersRegistry registry;
     [SerializeField] UICharacterCard uiCharacterCardPrefab;
     [SerializeField] Transform PlayerTeamContainer;
     [SerializeField] Transform NPCTeamContainer;
+
+    [SerializeField] GameObject WinScreenPrefab;
+    [SerializeField] GameObject DefeatScreenPrefab;
+
+    public void DisplayCombatEnd(bool isPeremoga)
+    {
+        GameObject prefab = isPeremoga ? WinScreenPrefab : DefeatScreenPrefab;
+        Instantiate(prefab, transform);
+    }
+
     public UICharacterCard CreateCard(Character character)
     {
         UICharacterCard newCard = Instantiate(uiCharacterCardPrefab, character.Team == 0 ? PlayerTeamContainer : NPCTeamContainer);
